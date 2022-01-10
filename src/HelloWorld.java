4161e66c1f6516b16ccbdae71d99f8d31a04b945
AdultJedi <93745774+AdultJedi@users.noreply.github.com>
2022-01-10 14:53:55 -0500

    Initial comment work 1 is done Work 2 is done Work 3 is also done!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Doing some work here");
-        System.out.println("Testing to make sure everything is connected");
-        System.out.println("Work 3 is done now!");
+        // test to see if I can push to GitHub
     }
 }
