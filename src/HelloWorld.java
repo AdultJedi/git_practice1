6c57a4d752335da2930d3ed70c700fe6d2b2f560
AdultJedi <93745774+AdultJedi@users.noreply.github.com>
2022-01-10 12:28:50 -0500

    Initial comment work 1 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Doing some work here");
+        System.out.println("Testing to make sure everything is connected");
     }
 }
